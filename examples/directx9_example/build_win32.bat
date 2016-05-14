d37ca827876b696136cc658cbf1d630e87f15fa3
Tang Zhixiong <dvorak4tzx@gmail.com>
2016-05-14 20:47:46 +0800

    lazy commit

--- examples/directx9_example/build_win32.bat
+++ examples/directx9_example/build_win32.bat
@@ -1,3 +1,7 @@
 @REM Build for Visual Studio compiler. Run your copy of vcvars32.bat or vcvarsall.bat to setup command-line compiler.
 mkdir Debug
-cl /nologo /Zi /MD /I ..\.. /I "%DXSDK_DIR%/Include" /D UNICODE /D _UNICODE *.cpp ..\..\*.cpp /FeDebug/directx9_example.exe /FoDebug/ /link /LIBPATH:"%DXSDK_DIR%/Lib/x86" d3d9.lib
+set DXSDK_DIR=C:/Program Files (x86)/Microsoft SDKs/Windows/v7.0A
+set DXSDK_INCLUDE_DIR=%DXSDK_DIR%/Include
+set DXSDK_LIB_DIR=%DXSDK_DIR%/Lib
+
+%comspec% /c"c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64 && cl.exe /nologo /Zi /MD /I ..\..\src\ImGui /I "%DXSDK_INCLUDE_DIR%" /D UNICODE /D _UNICODE *.cpp ..\..\src\ImGui\*.cpp /FeDebug/directx9_example.exe /FoDebug/ /link /LIBPATH:"%DXSDK_LIB_DIR%" d3d9.lib
---
+++ examples/opengl_example/README.md
@@ -0,0 +1,1 @@
+![imgui opengl windows vs2010 x64](http://whudoc.qiniudn.com/2016/imgui-opengl-win.png)
